6f6fa538821b3f608adbd1a1ffc1952a63d65829
David <dsi28@hotmail.com>
2019-03-20 16:23:39 -0400

    moved batch routes out of app.js into routes/batch.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,8 @@ mongoose = require('mongoose'),
 methodOverride = require('method-override'),
 expressSanitizer = require('express-sanitizer');
 
-//require models
-const Batch = require('./models/batch'),
-Item = require('./models/item');
+//route files
+const batchRoutes = require('./routes/batch');
 
 
 //app config
@@ -24,83 +23,7 @@ app.get('/', (req, res)=>{
     res.redirect('/batches');
 });
 
-app.get('/batches', (req, res)=>{
-    Batch.find({}, (err,foundBatches)=>{
-        if(err){
-            console.log(err);
-            res.redirect('back');
-        }else{
-            res.render('batches/index', {batches: foundBatches});
-        }
-    })
-});
-
-// new
-app.get('/batches/new', (req, res)=>{
-    res.render('batches/new');
-});
-
-app.post('/batches', (req,res)=>{
-    Batch.create({name: req.body.name}, (err,createdBatch)=>{
-        if(err){
-            console.log(err);
-            res.redirect('back');
-        }else{
-            res.redirect('/batches/'+createdBatch._id);
-        }
-    });
-})
-
-app.get('/batches/:id/edit', (req,res)=>{
-    Batch.findById(req.params.id, (err,foundBatch)=>{
-        if(err){
-            console.log(err);
-            res.redirect('back');
-        }else{
-            res.render('batches/edit', {batch: foundBatch});
-        }
-    })
-});
-
-app.put('/batches/:id', (req,res)=>{
-    Batch.findByIdAndUpdate(req.params.id, req.body.batch, (err,updatedBatch)=>{
-        if(err){
-            console.log(err);
-            res.redirect('back');
-        }else{
-            res.redirect('/batches/'+updatedBatch.id);
-        }
-    })
-});
-
-app.get('/batches/:id', (req,res)=>{
-    Batch.findById(req.params.id, (err,foundBatch)=>{
-        if(err){
-            console.log(err);
-            res.redirect('back');
-        }else{
-            res.render('batches/show', {batch: foundBatch});
-        }
-    })
-});
-
-app.delete('/batches/:id', (req,res)=>{
-    Batch.findByIdAndDelete(req.params.id, (err, deleteBatch)=>{
-        if(err){
-            console.log(err);
-            res.redirect('back');
-        }else{
-            Item.deleteMany({_id: {$in: deleteBatch.items}}, (err,deletedItems)=>{
-                if(err){
-                    console.log(err);
-                    res.redirect('back');
-                }else{
-                    res.redirect('/batches');
-                }
-            })
-        }
-    })
-})
+app.use('/batches', batchRoutes);
 
 
 app.listen(3000,()=>{console.log('App is alive...')})
---
+++ src/routes/batch.js
@@ -0,0 +1,88 @@
+const express = require('express'),
+router = express.Router({mergeParams: true}),
+Batch = require('../models/batch'),
+Item = require('../models/item');
+
+
+    // routes for :   batches/
+
+
+router.get('/', (req, res)=>{
+    Batch.find({}, (err,foundBatches)=>{
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            res.render('batches/index', {batches: foundBatches});
+        }
+    })
+});
+
+// new
+router.get('/new', (req, res)=>{
+    res.render('batches/new');
+});
+
+router.post('/', (req,res)=>{
+    Batch.create({name: req.body.name}, (err,createdBatch)=>{
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            res.redirect('/batches/'+createdBatch._id);
+        }
+    });
+})
+
+router.get('/:id/edit', (req,res)=>{
+    Batch.findById(req.params.id, (err,foundBatch)=>{
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            res.render('batches/edit', {batch: foundBatch});
+        }
+    })
+});
+
+router.put('/:id', (req,res)=>{
+    Batch.findByIdAndUpdate(req.params.id, req.body.batch, (err,updatedBatch)=>{
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            res.redirect('/batches/'+updatedBatch.id);
+        }
+    })
+});
+
+router.get('/:id', (req,res)=>{
+    Batch.findById(req.params.id, (err,foundBatch)=>{
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            res.render('batches/show', {batch: foundBatch});
+        }
+    })
+});
+
+router.delete('/:id', (req,res)=>{
+    Batch.findByIdAndDelete(req.params.id, (err, deleteBatch)=>{
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            Item.deleteMany({_id: {$in: deleteBatch.items}}, (err,deletedItems)=>{
+                if(err){
+                    console.log(err);
+                    res.redirect('back');
+                }else{
+                    res.redirect('/batches');
+                }
+            })
+        }
+    })
+});
+
+module.exports = router;
